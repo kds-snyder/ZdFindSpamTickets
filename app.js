8338f1ffcce38fcc2dcec7773498ecfc7181ffca
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-12-24 10:42:42 -0800

    calculate credentials

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 angular.module('app', [])
-    .controller('zdToolsController', function () {
-        var zdTools = this;
+    .controller('zdToolsController', function ($scope) {
+        //var zdTools = this;
+        var self = $scope;
+        self.zdEmailNotValid = false;
+
+        // Get Zendesk credentials: base64-encode [email + '/token:' + token]
+        self.getZdCredentials = function (email, token) {
+            var zdCredentials = btoa(email + '/token:' + token);
+            console.log('Credentials to encode: ' + email + '/token:' + token + ', encoded credentials: ' + zdCredentials);
+            return zdCredentials;
+        };
+
+        // Start checking tickets
+        self.startCheckTickets = function () {
+            if (!self.validateInput()) {
+                return;
+            }
+
+            self.zdCredentials = self.getZdCredentials(self.zdEmail, self.zdApiToken);
+            console.log('self.zdCredentials: ' + self.zdCredentials);
+        };
+
+        self.validateInput = function () {
+            return true;
+        };
 
     });
